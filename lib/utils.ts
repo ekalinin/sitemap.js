e4709d975726a8889d0b57924089ad4ad452af96
Rouven Weßling <rouven@contentful.com>
2019-07-13 09:28:57 -0700

    Remove dependency on lodash.chunk

--- lib/utils.ts
+++ lib/utils.ts
@@ -23,3 +23,32 @@ export function getTimestampFromDate (dt: Date, bRealtime?: boolean): string {
 
   return timestamp;
 }
+
+/**
+ * Based on lodash's implementation of chunk.
+ *
+ * Copyright JS Foundation and other contributors <https://js.foundation/>
+ *
+ * Based on Underscore.js, copyright Jeremy Ashkenas,
+ * DocumentCloud and Investigative Reporters & Editors <http://underscorejs.org/>
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals. For exact contribution history, see the revision history
+ * available at https://github.com/lodash/lodash
+ */
+export function chunk (array: any[], size = 1): any[] {
+  size = Math.max(Math.trunc(size), 0);
+
+  const length = array ? array.length : 0;
+  if (!length || size < 1) {
+    return [];
+  }
+  const result = Array(Math.ceil(length / size));
+  let index = 0,
+    resIndex = 0;
+
+  while (index < length) {
+    result[resIndex++] = array.slice(index, (index += size));
+  }
+  return result;
+}
