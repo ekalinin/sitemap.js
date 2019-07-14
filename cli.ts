c2452430d60a4a91400026cc8859660f14f1ac63
Patrick Weygand <patrick.weygand@gmail.com>
2019-07-14 15:03:47 -0700

    add support for multiple files

--- cli.ts
+++ cli.ts
@@ -1,6 +1,9 @@
 import { Sitemap } from './index'
 import { createInterface } from 'readline';
+import { Readable } from 'stream'
+import { createReadStream } from 'fs'
 console.warn('CLI is in new and likely to change quite a bit. Please send feature/bug requests to https://github.com/ekalinin/sitemap.js/issues')
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const arg = require('arg')
 
 const sm = new Sitemap()
@@ -8,6 +11,21 @@ const parseJSON = (line: string): number => (
   sm.add(JSON.parse(line))
 )
 const parseLine = (line: string): number => sm.add(line)
+
+async function processStreams (streams: Readable[], isJSON: boolean): Promise<string> {
+  for (let stream of streams) {
+    await new Promise((resolve): void => {
+      const rl = createInterface({
+        input: stream
+      });
+      rl.on('line', isJSON ? parseJSON : parseLine)
+      rl.on('close', (): void => {
+        resolve()
+      })
+    })
+  }
+  return sm.toString()
+}
 const argSpec = {
   '--help':    Boolean,
   '--version': Boolean,
@@ -15,16 +33,16 @@ const argSpec = {
 }
 const argv = arg(argSpec)
 if (argv['--version']){
+  /* eslint-disable-next-line @typescript-eslint/no-var-requires */
   const packagejson = require('../package.json')
   console.log(packagejson.version)
 } else if (argv['--help']) {
   console.log('TODO')
 } else {
-  const rl = createInterface({
-    input: process.stdin
-  });
-  rl.on('line', argv['--json'] ? parseJSON : parseLine)
-  rl.on('close', (): void => {
-    process.stdout.write(sm.toString())
-  })
+  processStreams(
+    argv._.map(
+      (file: string): Readable => createReadStream(file, { encoding: 'utf8' }))
+      .concat(process.stdin),
+    argv['--json']
+  )
 }
